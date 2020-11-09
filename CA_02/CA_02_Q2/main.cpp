7ec84e3b1af96d77b177ff19653a04cb0bbc762b
mohsenfayyaz <mohsen.fayyaz77@gmail.com>
2020-11-09 23:54:55 +0330

    Finish CA02

--- CA_02/CA_02_Q2/main.cpp
+++ CA_02/CA_02_Q2/main.cpp
@@ -3,6 +3,7 @@
 #include "x86intrin.h"
 #include <sys/time.h>
 #include <algorithm>
+#include <math.h>
 #include "util.cpp"
 #define VECTOR_SIZE 1048576  // 2^20 = 1048576
 
@@ -28,6 +29,44 @@ float scalar_mean(float* v1) {
 	return sum / VECTOR_SIZE;
 }
 
+float parallel_sd(float* v1){
+	float mean = parallel_mean(v1);
+	__m128 mean128 = _mm_set1_ps(mean);
+	__m128 sd = _mm_set1_ps(0.0);
+	__m128 vec;
+	for (long i = 0; i < VECTOR_SIZE; i += 4){
+		vec = _mm_load_ps(&(v1[i]));
+		vec = _mm_sub_ps(vec, mean128);
+		sd = _mm_add_ps(sd, _mm_mul_ps(vec, vec));
+	}
+	sd = _mm_hadd_ps(sd, sd);
+	sd = _mm_hadd_ps(sd, sd);
+	float final_sd = std::sqrt(_mm_cvtss_f32(sd) / VECTOR_SIZE);
+	return final_sd;
+}
+
+float scalar_sd(float* v1){
+	float mean = scalar_mean(v1);
+	float sd1 = 0;
+	float sd2 = 0;
+	float sd3 = 0;
+	float sd4 = 0;
+	for (long i = 0; i < VECTOR_SIZE; i+=4){
+		sd1 += (v1[i] - mean) * (v1[i] - mean);
+	}
+	for (long i = 1; i < VECTOR_SIZE; i+=4){
+		sd2 += (v1[i] - mean) * (v1[i] - mean);
+	}
+	for (long i = 2; i < VECTOR_SIZE; i+=4){
+		sd3 += (v1[i] - mean) * (v1[i] - mean);
+	}
+	for (long i = 3; i < VECTOR_SIZE; i+=4){
+		sd4 += (v1[i] - mean) * (v1[i] - mean);
+	}
+	float sd = sd1 + sd2 + sd3 + sd4;
+	return std::sqrt(sd / VECTOR_SIZE);
+}
+
 int main(void)
 {
 	print_names();
@@ -49,17 +88,18 @@ int main(void)
 	struct timeval start, end;
 
 	gettimeofday(&start, NULL);
-	float sres = scalar_mean(v1);
+	float sres = scalar_sd(v1);
 	gettimeofday(&end, NULL);
 	long seconds = (end.tv_sec - start.tv_sec);
 	long micros1 = ((seconds * 1000000) + end.tv_usec) - (start.tv_usec);
 
 	gettimeofday(&start, NULL);
-	float pres = parallel_mean(v1);
+	float pres = parallel_sd(v1);
 	gettimeofday(&end, NULL);
 	long seconds2 = (end.tv_sec - start.tv_sec);
 	long micros2 = ((seconds2 * 1000000) + end.tv_usec) - (start.tv_usec);
 
+	printf("\nStandard Deviation\n");
 	printf("\nThe serial result is  = %f\n", sres);
 	printf("The parallel result is = %f\n", pres);
 	printf("Serial Run time = %ld \n", micros1);
