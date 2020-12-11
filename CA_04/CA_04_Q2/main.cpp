1d173a17c8dfd0233b9fc366caaaa9d6c145b499
mahdi hadiloo <hadiloomehdi@gmail.com>
2020-12-11 23:26:04 +0330

    project 4 questions  2 solved

---
+++ CA_04/CA_04_Q2/main.cpp
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <math.h>
+#include <sys/time.h> 
+#include <sys/resource.h>  
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+ 
+
+// #define VECTOR_SIZE 1048576
+#define VECTOR_SIZE 100000
+
+
+using namespace std;
+
+void show(float* v){
+    for (long i = 0; i < VECTOR_SIZE; i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << "\n";
+    
+}
+
+void swap(float* v, long a, long b){
+    float temp = v[a];
+    v[a] = v[b];
+    v[b] = v[a];
+}
+
+long partition (float* v, long low, long high) 
+{ 
+    long p = low;
+    float pivot = v[low];
+    
+    for (long i = low + 1; i <= high; i++) 
+    { 
+        if (v[i] <= pivot) 
+        { 
+            p++;    
+            swap(v, i, p); 
+        } 
+    } 
+    swap(v, low, p); 
+    return p; 
+} 
+
+
+void quickSort(float* v, long left, long right){
+    if(left < right){
+        long p = partition(v, left, right);
+        quickSort(v, left, p -1);
+        quickSort(v, p + 1, right);
+    }
+}
+
+
+void scalar(float* v1){
+    quickSort(v1,0,VECTOR_SIZE - 1);
+
+}
+
+void parallel(float* v,long left ,long right){
+    #pragma omp parallel num_threads(10)
+	{
+		#pragma omp single
+		{
+			if(left < right){
+                long p = partition(v, left, right);
+                #pragma omp task
+                    quickSort(v, left, p -1);
+                    
+                #pragma omp task
+                    quickSort(v, p + 1, right);
+                    
+            }
+
+		}
+	}
+    
+    
+
+}
+
+int main(void){
+    vector<int> a;
+    
+    float v1[VECTOR_SIZE];
+    float v2[VECTOR_SIZE];
+    for (long i = 0; i < VECTOR_SIZE; i++){
+		float temp = static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / 100.0));
+        v1[i] = temp;
+        v2[i] = temp;   
+    }
+
+	struct timeval start, end;
+
+
+    printf("Mohsen Fayyaz - Mahdi Hadiloo\n");
+    printf("  810196650   -   810196587 \n");
+
+	gettimeofday(&start, NULL);
+	scalar(v1);
+	gettimeofday(&end, NULL);
+	// show(v1);
+    long seconds = (end.tv_sec - start.tv_sec);
+	long micros1 = ((seconds * 1000000) + end.tv_usec) - (start.tv_usec);
+
+	gettimeofday(&start, NULL);
+	parallel(v2, 0 , VECTOR_SIZE -1);
+	gettimeofday(&end, NULL);
+	// show(v2);
+    long seconds2 = (end.tv_sec - start.tv_sec);
+	long micros2 = ((seconds2 * 1000000) + end.tv_usec) - (start.tv_usec);
+
+	printf("Serial Run time = %ld \n", micros1);
+	printf("Parallel Run time = %ld \n", micros2);
+	printf("Speedup = %f\n\n", (float)(micros1) / (float)micros2);
+
+}
